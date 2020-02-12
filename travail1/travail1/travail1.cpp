a0619feae6b17e90263d8411ce77918ac2580ced
Jason Grondin <tijason@hotmail.com>
2020-02-12 15:24:11 -0500

    Commit debut distrubuer les cartes

--- travail1/travail1/travail1.cpp
+++ travail1/travail1/travail1.cpp
@@ -11,6 +11,10 @@ Donnees leJeu;
 
 void InitialiserJoueurs();
 void Jouer();
+int DemanderNbCarte();
+void MelangerCarte();
+void DistribuerLesCartes();
+
 
 int main() 
 {
@@ -28,8 +32,62 @@ int main()
 void Jouer() 
 {
 
+	DemanderNbCarte();
+	
 }
 
 void InitialiserJoueurs()
 {
+	string nomJoueur1;
+	string nomJoueur2;
+
+	cout << "Quelle est le nom du joueur 1 ?";
+	cin >> nomJoueur1;
+	leJeu.joueur1.Setnom(nomJoueur1);
+
+	cout << "Quelle est le nom du joueur 2 ?";
+	cin >> nomJoueur2;
+	leJeu.joueur2.Setnom(nomJoueur2);
+}
+int DemanderNbCarte()
+{
+	int nombreCarteParJoueur;
+	cout << "Combien de carte voulez-vous par usager?";
+	cin >> nombreCarteParJoueur;
+	return nombreCarteParJoueur;
+}
+
+void MelangerCarteTableau()
+{
+	int NombreAleatoire1 = 0;
+	int NombreAleatoire2 = 0;
+
+	srand(time(NULL));
+
+	Carte tempo;
+	
+		for (int i = 0; i < 3000; i++)
+		{
+			
+
+			NombreAleatoire1 = rand() % maxCartes;
+			NombreAleatoire2 = rand() % maxCartes;
+			tempo = leJeu.PaquetDeCarte[NombreAleatoire1];
+			leJeu.PaquetDeCarte[NombreAleatoire1] = leJeu.PaquetDeCarte[NombreAleatoire2];
+			leJeu.PaquetDeCarte[NombreAleatoire2] = tempo;
+		}
+}
+
+void DistribuerLesCartes(int nombreCarteParJoueur)
+{
+	int indice = 0;
+	for (indice = 0; indice < nombreCarteParJoueur; indice++)
+	{
+		if (indice % 2 == 0)
+		{
+			leJeu.joueur1.AjouterCarteAlaMain(Carte *indice)
+		}
+		}
+
+	leJeu.PaquetDeCarte
 }
